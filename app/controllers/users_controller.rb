9738d7cf532996a0dea816f1bae999a06c903f7d
AdityaGattu <gattuaditya@gmail.com>
2020-03-17 12:27:12 +0530

    final update for all users features

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+
+  def index
+    render plain: User.all.order(:id).map { |user| user.to_pleasant_string }.join("\n")
+  end
+
+  def show
+    id = params[:id]
+    user = User.find(id)
+    render plain: user.to_pleasant_string
+  end
+
+  def create
+    name = params[:name]
+    email = params[:email]
+    password = params[:password]
+    new_user = User.create!(
+      name: name,
+      email: email,
+      password: password,
+    )
+    response_text = "hey, new user created with id #{new_user.id}"
+    render plain: response_text
+  end
+
+  def login
+    email = params[:email]
+    password = params[:password]
+    verify_user = User.where(email: email, password: password).exists?
+    render plain: verify_user
+  end
+end
