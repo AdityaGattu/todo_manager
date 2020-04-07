917a0e9bbe56c50168d0beb65a294aeb3498eff3
AdityaGattu <gattuaditya@gmail.com>
2020-04-07 21:49:15 +0530

    updated migrations

--- app/models/Todo.rb
+++ app/models/Todo.rb
@@ -1,26 +1,4 @@
 class Todo < ActiveRecord::Base
-  def to_pleasant_string
-    is_completed = completed ? "[X]" : "[]"
-    "#{id}.#{due_date.to_s(:long)} #{todo_text} #{is_completed}"
-  end
-  def self.over_due
-    all.where("due_date < ?", Date.today)
-  end
-  def self.due_today
-    all.where("due_date = ?", Date.today)
-  end
-
-  def self.due_later
-    all.where("due_date > ?", Date.today)
-  end
-
-  def due_today?
-    due_date == Date.today
-  end
-
-  def self.completed
-    all.where(completed: true)
-  end
 
   def to_displayable_string
     display_status = completed ? "[X]" : "[ ]"
@@ -28,27 +6,34 @@ class Todo < ActiveRecord::Base
     "#{id}.  #{display_status} #{todo_text} #{display_date}"
   end
 
-  def self.show_list
-    puts "My Todo-list\n\n"
-
-    puts "Overdue\n"
-    puts over_due.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-
-    puts "Due Today\n"
-    puts due_today.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-
-    puts "Due Later\n"
-    puts due_later.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-  end
 
   def self.add_task(todo)
     create!(todo_text: todo[:todo_text], due_date: Date.today + todo[:due_in_days])
   end
 
-  def self.mark_as_complete!(todo_id)
-    update(todo_id, completed: true)
+  def self.due_today
+    all.where("due_date = ?", Date.today)
   end
+
+  def self.over_due
+    where("due_date < ? and (not completed)", Date.today)
+  end
+
+  def due_today?
+    due_date == Date.today
+  end
+
+  def self.due_later
+    all.where("due_date > ?", Date.today)
+  end
+
+  def self.completed
+    all.where(completed: true)
+  end
+
+  def self.not_completed
+    all.where(completed: false)
+  end
+
+
 end
