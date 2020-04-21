fbb6456d8af90858815e1f5f6322fe82eed9cd76
AdityaGattu <gattuaditya@gmail.com>
2020-04-21 12:14:40 +0530

    addedflash message

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
+  skip_before_action :ensure_user_logged_in
   def new
   end
 
   def create
     user = User.find(email: params[:email])
     if user && user.authenticate(params[:password])
-      render plain: "correct password"
+      session[:current_user_id] = user.id
+      redirect_to "/"
     else
-      render plain: "incorrect password"
+      flash[:error] = "Your login attempt is invalid"
+      redirect_to new_sessions_path
     end
   end
 end
