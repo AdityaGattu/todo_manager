73cbc404f0c306ced26e2dc23adb5cf5d071f8b7
AdityaGattu <gattuaditya@gmail.com>
2020-04-21 12:28:47 +0530

    added destroy session

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :todos
   resources :users
   get "/" => "home#index"
-  post "users/login", to: "users#login"
-
+  #post "users/login", to: "users#login"
   get "/signin" => "sessions#new", as: :new_sessions
   post "/signin" => "sessions#create", as: :sessions
   delete "/signout", to: "sessions#destroy", as: :destroy_session
