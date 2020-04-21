8723b5d000ce50319c77e5f595df5a8bd98da3cd
AdityaGattu <gattuaditya@gmail.com>
2020-04-21 15:49:01 +0530

    added valildations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base
   has_secure_password
   has_many :todos
 
-  validates :firstname, presence: true
+  validates :first_name, presence: true
   validates :email, presence: true
 
   def to_displayable_string
@@ -16,7 +16,6 @@ class User < ApplicationRecord
     password = newuser[:password_digest]
     new(first_name: first_name, last_name: last_name, email: email, password: password)
   end
-
   def self.check(credentials)
     where("gmail=? and password=?", credentials["email"], credentials["password"]).exists?
   end
